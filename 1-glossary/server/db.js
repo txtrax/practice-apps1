d908d7d51bfde5153e26cc20ce2f64340db46a7c
theresatran <theresa.tran216@gmail.com>
2022-03-24 13:15:15 -0700

    Added server get function

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,44 @@
 const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/glossary', {useNewUrlParser: true, useUnifiedTopology: true});
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+const friendSchema = new mongoose.Schema({
+  name: {type: String, unique: true},
+  phrase: String
+})
+
+const Friend = mongoose.model('Friend', friendSchema);
+
+// let friends = [
+//   {
+//     name: 'Theresa',
+//     phrase: 'Wait what?!'
+//   },
+//   {
+//     name: 'Annie',
+//     phrase: 'I cannot right now'
+//   },
+//   {
+//     name: 'Paul',
+//     phrase: 'WHAT DUHH'
+//   },
+//   {
+//     name: 'John',
+//     phrase: 'Please hold my hand'
+//   },
+// ]
+
+let create = (newObj) => {
+  let name = newObj.name;
+  let phrase = newObj.phrase;
+
+  let newFriend = new Friend( {name, phrase} );
+  newFriend.save();
+
+}
+
+module.exports.create = create;
+module.exports.Friend = Friend;
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,38 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require('./db.js');
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
 
-/**** 
- * 
- * 
- * Other routes here....
- *
- * 
- */
+//CONTROLLERS
+app.get('/friends', (req, res) => {
+  db.Friend.find({}).exec((err, result) => {
+    if (err) {
+      res.status(404).send(err);
+    } else {
+      res.status(200).send(result);
+    }
+  })
+})
+
+// app.post('/friends', ((req, res) => {
+//   console.log(req.body.name);
+
+//   db.create(req.query, (err, data) => {
+//     if (err) {
+//       res.status(404).send('error in server get')
+//     } else {
+
+
+//       res.status(200).send('We did it')
+//     }
+//   })
+// }))
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
